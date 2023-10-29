fd705854641b3f30a7a6db154e169c7b49604adf
Shukurulla <shukurullatursinbayev978@gmail.com>
2023-10-29 14:49:46 +0500

    change cors option

--- routers/news.js
+++ routers/news.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const News = require("../models/news");
 const router = express.Router();
+const cors = require("cors");
 
-router.get("/news", async (req, res) => {
+router.get("/news", cors(), async (req, res) => {
   const news = await News.find();
   res.json({ data: news });
 });
 
-router.post("/news", async (req, res) => {
+router.post("/news", cors(), async (req, res) => {
   await News.create(req.body);
   const news = await News.find();
   res.json({ data: news });
